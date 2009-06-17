686a955aa9876c81d3d6704bb6ed0d536c415f47
bhlbuu <bhlbuu@d245d934-5b62-11de-8abc-d1c337b90d21>
2009-06-17 19:12:00 +0000

    modified os separator

--- uuBrowser/src/__init__.py
+++ uuBrowser/src/__init__.py
@@ -2,3 +2,10 @@ import os
 BASEDIR=os.path.dirname(__file__)
 __version__='0.1 pre alpha'
 __author__={'buubui':'bhlbuu@apcs.vn'}
+
+sep=os.sep
+def generatePath(path):
+    if path:
+        return path.replace('/',sep)
+def pathFromBase(path):
+    return generatePath(BASEDIR+path)
